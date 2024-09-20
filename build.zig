04ceaf1489ee6009f4dbc5212084189ce5bb9dc6
x.x <38306136+xvfgc@users.noreply.github.com>
2024-09-20 14:57:28 -0300

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,30 +1,30 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const cpp_dep = b.dependency("cpp", .{ .target = target, .optimize = optimize });
+    const cppz_dep = b.dependency("cpp", .{ .target = target, .optimize = optimize });
 
     const lib = b.addStaticLibrary(.{
         .name = "cppz",
-        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    lib.root_module.addCSourceFile(.{
-        .file = cpp_dep.path("main.cpp"),
+    lib.addIncludePath(cppz_dep.path("."));
+    lib.addCSourceFile(.{
+        .file = cppz_dep.path("main.cpp"),
         .flags = &.{
-            "-fno-sanitize=undefined",
             "-std=c++11",
-            "-Wno-deprecated-declarations",
         },
     });
 
-    lib.addIncludePath(cpp_dep.path("."));
-    // lib.installHeadersDirectory(cpp_dep.path("."), "cppz", .{});
     lib.linkLibCpp();
+
+    _ = b.addModule("cppz", .{
+        .root_source_file = b.path("src/main.zig"),
+    });
 
     b.installArtifact(lib);
 }
